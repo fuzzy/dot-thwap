d10351857dfd2b724c555b07e1da7efd682d1516
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2019-06-04 09:53:46 -0700

    fixed this bit

--- etc/golang-packages.sh
+++ etc/golang-packages.sh
@@ -1,6 +1,10 @@
-fiximports:golang.org/x/tools/cmd/...
-godef:github.com/rogpeppe/godef/...
-gocode:github.com/nsf/gocode
-goimports:golang.org/x/tools/cmd/goimports
-guru:golang.org/x/tools/cmd/guru
-goflymake:github.com/dougm/goflymake
+for i in fiximports:golang.org/x/tools/cmd/... \
+             godef:github.com/rogpeppe/godef/... \
+             gocode:github.com/nsf/gocode \
+             goimports:golang.org/x/tools/cmd/goimports \
+             guru:golang.org/x/tools/cmd/guru \
+             goflymake:github.com/dougm/goflymake; do
+    provides=$(echo ${i}|awk -F: '{print $1}')
+    location=$(echo ${i}|awk -F: '{print $2}')
+    test -z "${provides}" && echo ${location} && go get ${location}
+done
